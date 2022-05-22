14fafa190350a76f7a0be96d39ca2b245197a4b7
Mateusz Mazurek <mauzer.official.1@gmail.com>
2022-05-22 20:12:11 +0200

    Update Droppable.kt

--- app/src/main/java/com/mauzerov/mobileplatform2/mvvm/dropdown/Droppable.kt
+++ app/src/main/java/com/mauzerov/mobileplatform2/mvvm/dropdown/Droppable.kt
@@ -1,41 +1,32 @@
 package com.mauzerov.mobileplatform2.mvvm.dropdown
 
-import android.annotation.SuppressLint
 import android.app.Activity
-import android.content.Context
-import android.os.Build
 import android.os.Handler
 import android.os.Looper
-import android.util.AttributeSet
-import android.util.DisplayMetrics
 import android.view.animation.AnimationUtils
 import android.widget.RelativeLayout
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import com.mauzerov.mobileplatform2.R
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
 
-@SuppressLint("ViewConstructor")
+
 abstract class Droppable constructor(context: Activity) :
     ConstraintLayout(context) {
-    private final val animationSlideUp = AnimationUtils.loadAnimation(context, R.anim.slide_up)
-    private final val animationSlideDown = AnimationUtils.loadAnimation(context, R.anim.slide_down)
+    private val animationSlideUp = AnimationUtils.loadAnimation(context, R.anim.slide_up)
+    private val animationSlideDown = AnimationUtils.loadAnimation(context, R.anim.slide_down)
 
     init {
         val panelLayout = RelativeLayout.LayoutParams(
             RelativeLayout.LayoutParams.MATCH_PARENT,
             RelativeLayout.LayoutParams.MATCH_PARENT
         ).apply {
-            leftMargin = 0
-            topMargin = 0
-            rightMargin = 0
-            bottomMargin = 0
+            setMargins(0, 0, 0, 0)
         }
 
         this.setBackgroundColor(
             ContextCompat.getColor( context, R.color.dark_black_overlay )
         )
+        @Suppress("LeakingThis")
         (context).addContentView(this, panelLayout)
 
         val duration = animationSlideUp.duration
@@ -44,26 +35,25 @@ abstract class Droppable constructor(context: Activity) :
         animationSlideUp.duration = duration
     }
 
-    private final fun onClose(): Long {
+    private fun onClose(): Long {
         this.startAnimation(animationSlideUp)
-
         Handler(Looper.getMainLooper()).postDelayed(
             { this.visibility = GONE }, animationSlideUp.duration
         )
         return animationSlideUp.duration
     }
 
-    private final fun onOpen(): Long {
+    private fun onOpen(): Long {
         this.visibility = VISIBLE
         this.startAnimation(animationSlideDown)
         return animationSlideDown.duration
     }
 
-    final fun open(): Long {
+    fun open(): Long {
         return this.onOpen()
     }
 
-    final fun close(): Long {
+    fun close(): Long {
         return this.onClose()
     }
 }
